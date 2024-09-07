4808adc168116c9f2f5f949e14d2543d474b9796
jayyi <eyjian@qq.com>
2024-09-07 21:36:41 +0800

    new

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,66 @@
 <template>
-  <div id="app">
-    <nav class="nav-bar">
-      <ul>
-        <li class="image-li">
-          <img :src="imgUrl" :alt="imgAlt" :width="imgWidth" :height="imgHeight">
-        </li>
-        <li><router-link to="/gcts">生成建表SQL</router-link></li>
-        <li><router-link to="/s2s">SQL转Struct</router-link></li>
-      </ul>
-    </nav>
-    <router-view/>
-  </div>
+
+    <div id="main-container">
+        <div id="nav-container">
+            <ul>
+                <li class="image-li">
+                    <img :src="imgUrl" :alt="imgAlt" :width="imgWidth" :height="imgHeight">
+                </li>
+                <li><router-link to="/gcts">生成建表SQL</router-link></li>
+                <li><router-link to="/s2s">SQL转Struct</router-link></li>
+            </ul>
+        </div>
+        <div id="content-container">
+        </div>
+    </div>
+    <div id="footer-container"></div>
+
 </template>
 
 <script>
+
 export default {
-  name: 'App',
-  data() {
-    return {
-      imgUrl: '/public/favicon.png',
-      imgAlt: 'Logo',
-      imgWidth: '64px',   // 设置图片宽度
-      imgHeight: '64px',  // 设置图片高度
-    };
-  },
-};
+    data() {
+        return {
+            imgUrl: '/public/favicon.png',
+            imgAlt: 'Logo',
+            imgWidth: '64px',   // 设置图片宽度
+            imgHeight: '64px',  // 设置图片高度
+        }
+    }
+}
+
 </script>
 
 <style>
 
-#app {
-  display: flex;
-  width: 1500px; /* 固定宽度 */
-  height: 1000px; /* 固定高度 */
+#main-container {
+    display: flex;
+    height: 800px;
+    border: 1px solid red; /* 设置细实线边框，颜色为蓝色 */
 }
 
-.nav-bar ul {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+#footer-container {
+    height: 100px;
+    border: 1px solid red; /* 设置细实线边框，颜色为蓝色 */
 }
 
-.nav-bar {
-  width: 200px;
-  background-color: #2c3e50;
-  padding: 20px;
-  color: white;
+#nav-container {
+    width: 200px;
+    border: 1px solid rgb(204, 255, 0); /* 设置细实线边框，颜色为蓝色 */
 }
 
-.nav-bar ul {
-  list-style-type: none;
-  padding: 0;
+#content-container {
+    flex-grow: 1; /* 使子元素填充剩余空间 */
+    border: 1px solid rgb(204, 255, 0); /* 设置细实线边框，颜色为蓝色 */
 }
 
-.nav-bar li {
-  margin-bottom: 10px;
+ul li {
+    list-style-type: none; /* 移除列表项前面的点 */
 }
 
-.router-link-active {
-  font-weight: bold;
+a {
+    text-decoration: none; /* 去掉超链接的下划线 */
 }
 
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import TheWelcome from '../components/TheWelcome.vue';
-import GenerateCreateTableSql from '../components/GenerateCreateTableSql.vue';
-import Sql2Struct from '../components/Sql2Struct.vue';
+
 
 const routes = [
-    {
-        path: '/',
-        redirect: '/gcts',
-    },
-    {
-        path: '/gcts',
-        name: 'gcts',
-        component: GenerateCreateTableSql
-    },
-    {
-        path: '/s2s',
-        name: 's2s',
-        component: Sql2Struct
-    }
+
 ];
 
 const router = createRouter({
